0df4a26f6679442d2c93762129cab37d07349dde
Jasperhino <blum.jasper@gmail.com>
2022-06-13 12:47:34 +0200

    Try to write blob from action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,23 +3,25 @@
 # Use tokei to generate metrics.json
 echo $(tokei ./src -o json > metrics.json)
 
-TOKEN="${GITHUB_TOKEN:-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}"
-echo $TOKEN
-exit 0
+if [[ -z "${GITHUB_TOKEN}" ]]; then
+  echo "GITHUB_TOKEN is not set. Stopping Hiviser Action."
+  exit 0
+fi
+
 OWNER="hpicgs"
 REPO="github-software-analytics-embedding"
 
-IMG_PATH="sample.png"
-PAYLOAD="{\"content\": \"$(base64 -w 0 $IMG_PATH)\", \"encoding\": \"base64\"}"
-echo $PAYLOAD > "$IMG_PATH.json"
-
 # Create BLOB for the metrics
-create_blob=$(curl -X POST -H "Authorization: token $TOKEN" -H "Content-Type: application/json" -d @metrics.json "https://api.github.com/repos/$OWNER/$REPO/git/blobs")
+create_blob=$(curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/json" -d '{"content":"This is a test blob","encoding":"utf-8"}' "https://api.github.com/repos/$OWNER/$REPO/git/blobs")
 echo $create_blob
+
+#IMG_PATH="sample.png"
+#PAYLOAD="{\"content\": \"$(base64 -w 0 $IMG_PATH)\", \"encoding\": \"base64\"}"
+#echo $PAYLOAD > "$IMG_PATH.json"
 
 # Create BLOB for the image
-create_blob=$(curl -X POST -H "Authorization: token $TOKEN" -H "Content-Type: application/json" -d @$IMG_PATH.json "https://api.github.com/repos/$OWNER/$REPO/git/blobs")
-echo $create_blob
+##create_blob=$(curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/json" -d @$IMG_PATH.json "https://api.github.com/repos/$OWNER/$REPO/git/blobs")
+#echo $create_blob
 
 
 #FILE_SHA="b304b71d8cf7a709831b4b2e0842c6218cd96fe5"
