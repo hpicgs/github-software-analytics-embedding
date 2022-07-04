dac5a72c9fe4448ea9a41476114170b372eb70a9
Jasperhino <blum.jasper@gmail.com>
2022-07-04 15:04:42 +0200

    Add fetch for metrics refs to home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '@mui/material'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
+import { getMetricCommits, RefsResponse } from '@/utils/github'
+import { Endpoints } from '@octokit/types'
 
 export default function Home() {
   const navigate = useNavigate();
+  const [data, setData] = useState<RefsResponse>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getMetricCommits('hpicgs', 'github-software-analytics-embedding')
+      setData(data)
+    }
+    fetchData()
+  }, [])
+
   return (
     <div>
         <Button variant="outlined"
--- frontend/src/utils/github.ts
+++ frontend/src/utils/github.ts
@@ -1,37 +1,62 @@
 import { Octokit } from "octokit";
 import { Buffer } from "buffer";
+import { Endpoints } from "@octokit/types";
 const octokit = new Octokit();
 
+export async function getMetrics(
+  owner: string,
+  repo: string,
+  commit_sha: string
+): Promise<string> {
+  const ref = await octokit.rest.git.getRef({
+    owner,
+    repo,
+    ref: `metrics/${commit_sha}`,
+  });
 
-export async function getMetrics(owner: string, repo: string, commit_sha: string): Promise<string> {
-    const ref = await octokit.rest.git.getRef({
-        owner,
-        repo,
-        ref: `metrics/${commit_sha}`
-    })
-        
-    const tree_sha = ref.data.object.sha
-    const response = await octokit.rest.git.getTree({
-        owner,
-        repo,
-        tree_sha
-    })
+  const tree_sha = ref.data.object.sha;
+  const response = await octokit.rest.git.getTree({
+    owner,
+    repo,
+    tree_sha,
+  });
 
-    response.data.tree.map((object) => {
-        console.log(object.path)
-    })
+  response.data.tree.map((object) => {
+    console.log(object.path);
+  });
 
-    const metrics_sha = response.data.tree.find((object) => object.path === 'metrics.csv')?.sha
+  const metrics_sha = response.data.tree.find(
+    (object) => object.path === "metrics.csv"
+  )?.sha;
 
-    if (!metrics_sha) throw new Error('metrics.csv not found in tree')
+  if (!metrics_sha) throw new Error("metrics.csv not found in tree");
 
-    const metrics_blob = await octokit.rest.git.getBlob({
-        owner,
-        repo,
-        file_sha: metrics_sha
-    })
+  const metrics_blob = await octokit.rest.git.getBlob({
+    owner,
+    repo,
+    file_sha: metrics_sha,
+  });
 
-    const metrics_csv = Buffer.from(metrics_blob.data.content, 'base64').toString()
-    console.log(metrics_csv)
-    return metrics_csv
+  const metrics_csv = Buffer.from(
+    metrics_blob.data.content,
+    "base64"
+  ).toString();
+  console.log(metrics_csv);
+  return metrics_csv;
+}
+
+export type RefsResponse =
+  Endpoints["GET /repos/{owner}/{repo}/git/matching-refs/{ref}"]["response"];
+
+export async function getMetricCommits(
+  owner: string,
+  repo: string
+): Promise<RefsResponse> {
+  const response = await octokit.rest.git.listMatchingRefs({
+    owner,
+    repo,
+    ref: "metrics",
+  });
+  console.log(response);
+  return response;
 }
