73b5fce633b31f66dcea01a95dacdd296d44cc3d
Franziska Lauterbach <franziskalauterbach99@gmail.com>
2022-08-28 16:54:42 +0200

    changed metric calculation to typescript compiler

--- analytics/src/index.ts
+++ analytics/src/index.ts
@@ -1,7 +1,15 @@
-import { parseScript, Program, tokenize } from "esprima";
 import { readFileSync, writeFileSync } from "fs";
 import { promisify } from "util";
 import glob from "glob";
+import {
+  createSourceFile,
+  Node,
+  ScriptTarget,
+  SyntaxKind,
+  forEachChild,
+  getLeadingCommentRanges,
+  CommentRange,
+} from "typescript";
 import { saveMetrics } from "./github/api";
 
 const globPromise = promisify(glob);
@@ -16,12 +24,18 @@ async function processFiles() {
 
   const csvData = [];
   for (const filename of files) {
+    console.log(`Currently analysing ${filename}`);
     const file = readFileSync(filename, "utf-8");
-    const ast = parseScript(file, { loc: true, comment: true, range: true });
-    //const tokens = tokenize(file, { loc: true });
+    const ast = createSourceFile(
+      filename,
+      file.toString(),
+      ScriptTarget.Latest
+    );
+
+    const comments = getComments(ast);
     const loc = getLoC(ast);
-    const noc = getNoC(ast);
-    const cloc = getCommentLines(ast);
+    const noc = getNoC(comments);
+    const cloc = getCommentLines(comments, ast.getFullText());
     const dc = getDensityOfComments(loc + cloc, cloc);
     const functionNumber = getNumberOfFunctions(ast);
     csvData.push([filename, loc, noc, cloc, dc, functionNumber]);
@@ -43,12 +57,9 @@ processFiles().catch((e) => {
 Metric: Lines of Code
 TODO: eliminate blank lines -> when parsing the file probably
 */
-function getLoC(ast: Program): number {
-  let loc = -1;
-  if (ast.loc?.end && ast.loc?.start) {
-    loc = ast.loc?.end?.line - ast.loc?.start?.line;
-  }
-  return loc;
+function getLoC(ast: Node): number {
+  const fullText = ast.getFullText();
+  return (fullText.match(/\n/g) || "").length + 1;
 }
 
 /*
@@ -57,8 +68,30 @@ Metric: Numbers of Comments
 -> single-line comments count as 1
 -> multi-line comments count as 1
 */
-function getNoC(ast: Program): number {
-  return ast.comments?.length || 0;
+function getComments(ast: Node) {
+  let comments: CommentRange[] = [];
+  const fullText = ast.getFullText();
+  function visit(node: Node) {
+    forEachChild(node, visit);
+
+    const commentRange = getLeadingCommentRanges(fullText, node.getFullStart());
+    if (commentRange) {
+      comments = comments.concat(commentRange);
+    }
+  }
+  forEachChild(ast, visit);
+  const uniqueComments = comments.reduce((acc: CommentRange[], curr) => {
+    if (!acc.some((t) => curr.end === t.end && curr.pos === t.pos)) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
+
+  return uniqueComments;
+}
+
+function getNoC(comments: CommentRange[]): number {
+  return comments.length;
 }
 
 /*
@@ -67,11 +100,13 @@ Metric: Comment Lines
 -> single-line count as 1
 -> multi-line comments count as multiple lines 
 */
-function getCommentLines(ast: Program): number {
-  const comments: object = ast.comments || {};
+
+function getCommentLines(comments: CommentRange[], fullText: string): number {
   let commentLines = 0;
-  for (const [_, value] of Object.entries(comments)) {
-    commentLines += value.loc?.end?.line - value.loc?.start?.line + 1;
+  for (const comment of comments) {
+    commentLines +=
+      (fullText.substring(comment.pos, comment.end).match(/\n/g) || "").length +
+      1;
   }
   return commentLines;
 }
@@ -90,12 +125,27 @@ Metric: Nesting Level (NL)
 TODO: implement evalutating of nesting level (NL)
 */
 
-function getNumberOfFunctions(ast: Program): number {
+/*
+MethodDeclaration: class member
+FunctionDeclaration: function Keyword
+ArrowFUnction: ArrowFUnction
+
+
+- get set Accessor? 
+- Constructor?
+*/
+function getNumberOfFunctions(ast: Node): number {
   let numberOfFunctions = 0;
-  for (const [key, value] of Object.entries(ast.body)) {
-    if (value.type === "FunctionDeclaration") {
+  function visit(node: Node) {
+    forEachChild(node, visit);
+    if (
+      node.kind === SyntaxKind.MethodDeclaration ||
+      node.kind === SyntaxKind.FunctionDeclaration ||
+      node.kind === SyntaxKind.ArrowFunction
+    ) {
       numberOfFunctions++;
     }
   }
+  forEachChild(ast, visit);
   return numberOfFunctions;
 }
