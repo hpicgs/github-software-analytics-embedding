a3015d0c50283c73c237d98e0c7a14d4b220c7d9
Jasperhino <blum.jasper@gmail.com>
2022-10-03 14:22:53 +0200

    Add benchmarking option

--- analytics/src/index.ts
+++ analytics/src/index.ts
@@ -4,10 +4,11 @@ import { promisify } from "util";
 import { storeMetricsToRepo } from "./github";
 import { createObjectCsvStringifier } from "csv-writer";
 import { getMetrics } from "./metrics";
+import { performance } from "perf_hooks";
 
 const globPromise = promisify(glob);
 
-async function analyseRepository() {
+async function analyseRepository(benchmark = false) {
   if (!process.env.REPOSITORY_PATH)
     throw new Error("REPOSITORY_PATH environment variable is not set");
   console.log(`REPOSITORY_PATH="${process.env.REPOSITORY_PATH}"`);
@@ -26,18 +27,41 @@ async function analyseRepository() {
     ],
   });
 
-  const metrics = files.map((filename) => ({
-    filename,
-    ...getMetrics(filename),
-  }));
+  let timings: { [filename: string]: number } = {};
+  if (benchmark) console.time("calculating-metrics");
+  const metrics = files.map((filename) => {
+    if (benchmark) {
+      const start = performance.now();
+      const m = {
+        filename,
+        ...getMetrics(filename),
+      };
+      const end = performance.now();
+      timings[filename] = end - start;
+      return m;
+    } else {
+      return {
+        filename,
+        ...getMetrics(filename),
+      };
+    }
+  });
+  if (benchmark) console.timeEnd("calculating-metrics");
+  if (benchmark) console.log(timings);
+  if (benchmark) console.time("writing-csv");
+
   const header = csvStringifier.getHeaderString();
   const csv = header + csvStringifier.stringifyRecords(metrics);
-
   writeFileSync("metrics.csv", csv);
-  storeMetricsToRepo(csv);
+
+  if (benchmark) console.timeEnd("writing-csv");
+  if (benchmark) console.time("storing-in-github");
+  await storeMetricsToRepo(csv);
+  if (benchmark) console.timeEnd("storing-in-github");
 }
 
-analyseRepository().catch((e) => {
+const benchmark = Boolean(process.env.BENCHMARK);
+analyseRepository(benchmark).catch((e) => {
   console.error(e);
   process.exit(1);
 });
