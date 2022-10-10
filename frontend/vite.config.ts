5a4f95597aa80b53b4c7354069be2012d48c27a6
Jasperhino <blum.jasper@gmail.com>
2022-10-10 17:08:09 +0200

    Fix github pages build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ command, mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
-  const base = process.env.VITE_BASE_NAME
-    ? `/${process.env.VITE_BASE_NAME}/`
-    : "/";
   const config: UserConfig = {
-    base,
     plugins: [react(), tsconfigPaths()],
     resolve: {
       alias: {
