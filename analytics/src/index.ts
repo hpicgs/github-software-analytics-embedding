aa89b807e046444b1b3691b10fc2e279ffa350e8
Jasperhino <blum.jasper@gmail.com>
2024-01-25 10:42:19 +0100

    remove ts analystics, rename package to storage

--- analytics/src/index.ts
+++ analytics/src/index.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 
-//import { analyseRepository } from "./typescript-analysis";
 import { storeInGitHub } from "./github-storage";
 
 if (!process.env.REPOSITORY_PATH) {
@@ -17,11 +16,6 @@ console.log(`METRICS_PATH="${process.env.METRICS_PATH}"`)
 
 const benchmark = Boolean(process.env.BENCHMARK);
 console.log(`Perform benchmarking of operations`);
-
-// analyseRepository(process.env.REPOSITORY_PATH, 'metrics.csv', benchmark).catch((e) => {
-//   console.error(e);
-//   process.exit(1);
-// });
 
 storeInGitHub(metrics_path, benchmark).catch((e) => {
   console.error(e);
